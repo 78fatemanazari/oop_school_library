8670d031e5ccf807185e9fccb9250c5e3cd75c00
fatemanazari <fatemanazari78@gmail.com>
2024-01-05 22:53:58 +0330

    Add association between classroom and student classes

--- student.rb
+++ student.rb
@@ -1,11 +1,16 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
+  def initialize(age, name: 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
+  end
+
+  def assign_to_classroom(classroom)
     @classroom = classroom
+    classroom.add_student(self)
   end
 
   def play_hooky
